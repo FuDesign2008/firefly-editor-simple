3ef42012b16e0fe404981bf2edac8af398267036
fudesign2008 <fudesign2008@163.com>
2018-07-13 16:19:05 +0800

    modified:   package-lock.json   modified:   package.json   new file:   src/main.js

---
+++ src/main.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author fuyg
+ * @date  2018-07-13
+ */
+import EventEmitter from 'wolfy87-eventemitter'
+
+class FireflyEditor extends EventEmitter {
+  constructor(/*options*/) {
+    super()
+    // TODO
+  }
+
+  setContent() {
+    // TODO
+  }
+
+  getContent() {
+    // TODO
+  }
+
+  execCommand() {
+    // TODO
+  }
+}
+
+export default FireflyEditor
